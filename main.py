f7bb7255ab2a2a418c53ca71cd18491af8c1db05
roadroller70ru <wrr@nm.ru>
2022-02-10 15:42:10 +0700

    1

--- main.py
+++ main.py
@@ -2,51 +2,25 @@ import platform
 import socket
 from getmac import get_mac_address
 import getpass
-import os
-import psutil
+import pandas as pd
+import xlsxwriter
 if platform.uname().system == 'Windows':
     import winapps
     import wmi
     w = wmi.WMI()
 global list
+global dict
 list=[]
+pc_dict={}
 
-"""
-def main():
-    global path
-    path= "c:/systeminfo"
-    for BIOSs in w.Win32_ComputerSystem():
-        UserNames=BIOSs.Caption
-    fileName=path+os.path.sep+UserNames+".txt"
-    info()
-
-# Оценить, существует ли папка (путь)
-    if not os.path.exists(path):
-        print ("не существует")
-# Создать папку (путь к файлу)
-        os.makedirs(path)
-# Записать информацию о файле
-        with open(fileName,'w+') as f:
-            for li in list:
-                print(li)
-                l=li+"\n"
-                f.write(l)
-    else:
-        print ("существует")
-        with open(fileName,'w+') as f:
-            for li in list:
-                print(li)
-                l=li+"\n"
-                f.write(l)
-"""
 def info():
-    list.append("Информация о компьютере")
-    list.append("Операционная система: % s" % platform.uname().system)
-    list.append("Версия ОС: % s" % platform.uname().version)
-    list.append("Имя компьютера: % s" % platform.node())
-    list.append("IP адресс ПК: % s" % socket.gethostbyname_ex(socket.gethostname())[-1][-1])
-    list.append("MAC адресс: % s" % get_mac_address(ip = socket.gethostbyname_ex(socket.gethostname())[-1][-1]))
-    list.append("Текущий пользователь: % s" % getpass.getuser())
+    pc_dict.clear()
+    pc_dict.update({"Операционная система:" : platform.uname().system})
+    pc_dict.update({"Версия ОС:" : platform.uname().version})
+    pc_dict.update({"Имя компьютера:" : platform.node()})
+    pc_dict.update({"IP адресс ПК:": socket.gethostbyname_ex(socket.gethostname())[-1][-1]})
+    pc_dict.update({"MAC адресс:": get_mac_address(ip = socket.gethostbyname_ex(socket.gethostname())[-1][-1])})
+    pc_dict.update({"Текущий пользователь:": getpass.getuser()})
 
     if platform.uname().system == 'Windows':
         indx = 0
@@ -60,16 +34,25 @@ def info():
                 if devname in tagReplacedSlashes:
                     sernum = x.SerialNumber
 
-            list.append("Марка диска:% s" % disk.Caption)
-            list.append("Серийный номер диска: % s" % sernum)
-            list.append("Размер диска:% .2fGB" % (diskSize / 1024 ** 3))
+            pc_dict.update({"Марка диска:" : disk.Caption})
+            pc_dict.update({"Серийный номер диска:" : sernum})
+            pc_dict.update({"Размер диска:" : (diskSize / 1024 ** 3)})
 
-        for li in list:
-            print(li)
-            l = li + "\n"
-        print("\n" + "Установленное ПО:")
+        pc_df = pd.DataFrame(pc_dict.items())
+
         for app in winapps.list_installed():
-             print(app)
+            list.append(app)
+
+        po_df = pd.DataFrame({'col' : list})
+
+        s_sheets = {'Система': pc_df, 'ПО': po_df}
+        writer = pd.ExcelWriter('C:/tmp/test.xlsx', engine='xlsxwriter')
+
+        for sheet_name in s_sheets.keys():
+            s_sheets[sheet_name].to_excel(writer, sheet_name=sheet_name, index=False)
+
+        writer.save()
+
 
 
 def main():
