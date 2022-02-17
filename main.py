684ccceb71f670184e7a9acdd197b4969ebe6f08
roadroller70ru <wrr@nm.ru>
2022-02-17 10:13:32 +0700

    1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import socket
 from getmac import get_mac_address
 import getpass
 import pandas as pd
+import re
 import xlsxwriter
 if platform.uname().system == 'Windows':
     import winapps
@@ -10,7 +11,8 @@ if platform.uname().system == 'Windows':
     w = wmi.WMI()
 global list
 global dict
-list=[]
+lnameapp=[]
+lverapp=[]
 pc_dict={}
 
 def info():
@@ -41,9 +43,36 @@ def info():
         pc_df = pd.DataFrame(pc_dict.items())
 
         for app in winapps.list_installed():
-            list.append(app)
+            #print(app)
+            #InstalledApplication(name='Windows Driver Package - DEVLINE LTD (DVRSE) Media  (05/13/2015 )', version='05/13/2015 ', install_date=None, install_location=None, install_source=None, modify_path=None, publisher='DEVLINE LTD', uninstall_string='C:\\PROGRA~1\\DIFX\\B60D12~1\\dpinst.exe /u C:\\Windows\\System32\\DriverStore\\FileRepository\\dvrs.inf_amd64_850a0d787d994d4b\\dvrs.inf')
+            res = re.split(r"'", str(app))
+            i = 0
+            while i < len(res):
+                #     print(res[i])
 
-        po_df = pd.DataFrame({'col' : list})
+                if re.search(r'name=', res[i]):
+                    #print('name= ' + res[i + 1])
+                    lnameapp.append(res[i+1])
+                    break
+                i += 1
+
+            while i < len(res):
+                if re.search(r'version=None', res[i]):
+                    #print('version= ' + res[i + 1])
+                    lverapp.append("None")
+                    break
+                else:
+                    if re.search(r'version=', res[i]):
+                        # print('version= ' + res[i + 1])
+                        lverapp.append(res[i+1])
+                        break
+                i += 1
+            #list.append(appname)
+
+        #print(len(lnameapp))
+        #print(len(lverapp))
+
+        po_df = pd.DataFrame({'AppName' : lnameapp, 'Version' : lverapp})
 
         s_sheets = {'Система': pc_df, 'ПО': po_df}
         writer = pd.ExcelWriter('C:/tmp/test.xlsx', engine='xlsxwriter')
@@ -52,8 +81,6 @@ def info():
             s_sheets[sheet_name].to_excel(writer, sheet_name=sheet_name, index=False)
 
         writer.save()
-
-
 
 def main():
     info()
